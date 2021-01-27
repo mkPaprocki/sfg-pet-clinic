4837658aa57a4f5a509e2a8d7331a963ad094154
mkPaprocki <mk.paprocki@gmail.com>
2021-01-27 06:29:56 +0100

    Refactored Map Impl to abstract ID generation. Closes #23

--- pet-clinic-web/src/main/java/com/github/mkpaprocki/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/github/mkpaprocki/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,4 +1,4 @@
-package com.github.mkpaprocki.sfgpetclinic;
+package com.github.mkpaprocki.sfgpetclinic.bootstrap;
 
 import com.github.mkpaprocki.sfgpetclinic.model.Owner;
 import com.github.mkpaprocki.sfgpetclinic.model.Vet;
@@ -30,14 +30,12 @@ public class DataLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Fiona");
         owner2.setLastName("Gleanne");
 
@@ -46,14 +44,12 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loaded Owners...");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Sam");
         vet1.setLastName("Axe");
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Jessie");
         vet2.setLastName("Porter");
 
