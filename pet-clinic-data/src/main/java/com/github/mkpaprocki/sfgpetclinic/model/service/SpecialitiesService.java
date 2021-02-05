ccec32480fcd9557c762af9583b12a0338b54eec
mkPaprocki <mk.paprocki@gmail.com>
2021-02-05 06:38:26 +0100

    added Specialities Map Impl. closes #31

---
+++ pet-clinic-data/src/main/java/com/github/mkpaprocki/sfgpetclinic/model/service/SpecialitiesService.java
@@ -0,0 +1,13 @@
+package com.github.mkpaprocki.sfgpetclinic.model.service;
+
+import com.github.mkpaprocki.sfgpetclinic.model.Speciality;
+
+/**
+ * Project: sfg-pet-clinic
+ * <p>
+ * Created on: 05.02.2021
+ * <p>
+ * Author    : Mateusz Paprocki
+ */
+public interface SpecialitiesService extends CrudService<Speciality, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/github/mkpaprocki/sfgpetclinic/model/service/map/SpecialityMap.java
@@ -0,0 +1,42 @@
+package com.github.mkpaprocki.sfgpetclinic.model.service.map;
+
+import com.github.mkpaprocki.sfgpetclinic.model.Speciality;
+import com.github.mkpaprocki.sfgpetclinic.model.service.SpecialitiesService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+/**
+ * Project: sfg-pet-clinic
+ * <p>
+ * Created on: 05.02.2021
+ * <p>
+ * Author    : Mateusz Paprocki
+ */
+@Service
+public class SpecialityMap extends AbstractMapService<Speciality, Long> implements SpecialitiesService {
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+    }
+
+    @Override
+    void deleteById(Long id) {
+        map.remove(id);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+}
