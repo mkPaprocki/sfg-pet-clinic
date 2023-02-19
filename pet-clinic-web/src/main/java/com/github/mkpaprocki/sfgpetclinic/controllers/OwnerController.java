f12f5d43c3397f3b52e58f33188437db3ff5eebd
Mateusz Paprocki <mpaprocki@si-ecommerce.pl>
2023-02-19 14:12:41 +0100

    added htmp templates. added missing css styles. Modified owner controller. added tests

--- pet-clinic-web/src/main/java/com/github/mkpaprocki/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/github/mkpaprocki/sfgpetclinic/controllers/OwnerController.java
@@ -4,7 +4,9 @@ import com.github.mkpaprocki.sfgpetclinic.service.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Project: sfg-pet-clinic
@@ -28,11 +30,18 @@ public class OwnerController {
 
         model.addAttribute("owners", ownerService.findAll());
 
-        return "/owners/index";
+        return "owners/index";
     }
 
     @RequestMapping("/find")
     public String findOwners() {
-        return "notimplemented";
+        return "owners/findOwners";
+    }
+
+    @GetMapping("/{ownerId}")
+    public ModelAndView showOwner(@PathVariable("ownerId") Long ownerId) {
+        ModelAndView mav = new ModelAndView("owners/ownerDetails");
+        mav.addObject(ownerService.findById(ownerId));
+        return mav;
     }
 }
