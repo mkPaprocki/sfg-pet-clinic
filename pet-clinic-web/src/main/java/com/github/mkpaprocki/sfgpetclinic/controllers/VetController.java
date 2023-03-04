4799f0f803c864ede0910e0d71dee22d80250981
Mateusz Paprocki <mpaprocki@si-ecommerce.pl>
2023-03-04 11:49:10 +0100

    added json endpoint closes #75

--- pet-clinic-web/src/main/java/com/github/mkpaprocki/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/github/mkpaprocki/sfgpetclinic/controllers/VetController.java
@@ -1,9 +1,14 @@
 package com.github.mkpaprocki.sfgpetclinic.controllers;
 
+import com.github.mkpaprocki.sfgpetclinic.model.Vet;
 import com.github.mkpaprocki.sfgpetclinic.service.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 /**
  * Project: sfg-pet-clinic
@@ -21,11 +26,16 @@ public class VetController {
         this.vetService = vetService;
     }
 
-    @RequestMapping({"/vets", "/vets/index", "/vets/index.html","/vets.html"})
+    @RequestMapping({"/vets", "/vets/index", "/vets/index.html", "/vets.html"})
     String listVets(Model model) {
 
         model.addAttribute("vets", vetService.findAll());
 
         return "vets/index";
+    }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJson() {
+        return vetService.findAll();
     }
 }
